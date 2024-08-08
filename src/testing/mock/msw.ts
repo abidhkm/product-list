3a230e4b954d408f6cad5b3f14754069fc27e7e9
Abidh <abidh.muhammed@erblotse.de>
2024-08-08 12:50:22 +0200

    use deployed json server

--- src/testing/mock/msw.ts
+++ src/testing/mock/msw.ts
@@ -2,31 +2,34 @@ import { SetupServer, setupServer } from 'msw/node'
 import { rest } from 'msw/'
 
 export const MockRequestHandler = [
-    rest.get(`http://localhost:3001/products`, (req, res, ctx) => {
-        return res(
-            ctx.status(200),
-            ctx.json([
-                {
-                    id: 1,
-                    name: 'Test Product 1',
-                    category: 'Category A',
-                    price: 10,
-                },
-                {
-                    id: 2,
-                    name: 'Test Product 2',
-                    category: 'Category A',
-                    price: 30,
-                },
-                {
-                    id: 33,
-                    name: 'Test Product 3',
-                    category: 'Category B',
-                    price: 90.99,
-                },
-            ])
-        )
-    }),
+    rest.get(
+        `https://json-server-two-mu.vercel.app/products`,
+        (req, res, ctx) => {
+            return res(
+                ctx.status(200),
+                ctx.json([
+                    {
+                        id: 1,
+                        name: 'Test Product 1',
+                        category: 'Category A',
+                        price: 10,
+                    },
+                    {
+                        id: 2,
+                        name: 'Test Product 2',
+                        category: 'Category A',
+                        price: 30,
+                    },
+                    {
+                        id: 33,
+                        name: 'Test Product 3',
+                        category: 'Category B',
+                        price: 90.99,
+                    },
+                ])
+            )
+        }
+    ),
 ]
 
 export const server: SetupServer = setupServer(...MockRequestHandler)
